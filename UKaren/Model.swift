df134bfd8e68f61ddbfd43e1fab7f4212822b7bd
ehwah <soewah1996@gmail.com>
2020-07-01 20:34:41 -0400

    working on parsing

---
+++ UKaren/Model.swift
@@ -0,0 +1,15 @@
+//
+//  Model.swift
+//  UKaren
+//
+//  Created by sowah on 7/1/20.
+//  Copyright © 2020 sowah. All rights reserved.
+//
+
+import Foundation
+
+struct Word: Codable {
+    let name: String
+    let andDescription: String
+    let detail: String
+}
--- UKaren/ViewController.swift
+++ UKaren/ViewController.swift
@@ -8,13 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    var data = [Word]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        parseJson()
     }
-
-
+    
+    func parseJson() {
+        guard let urlString = Bundle.main.url(forResource: "DataFile.json", withExtension: nil) else {
+            fatalError("Failed to locate in bundle.")
+        }
+        
+        if let data = try? Data(contentsOf: urlString) {
+            // we're OK to parse!
+            
+            if let json = try? JSONDecoder().decode([Word].self, from: data) {
+                json.forEach { (word) in
+                    self.data.append(word)
+                    
+                }
+            }
+            tableView.reloadData()
+            
+        }
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row].name
+        return cell
+    }
+    
 }
+
 
